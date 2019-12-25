d789729cacf1d5d1e564b27cdbe20c726e7a4423
siddhant3030 <ssiddhant3030@gmail.com>
2019-12-26 04:14:30 +0530

    objects added

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput placeholder="Course Goal" style={{width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}/>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input}/>
         <Button title="ADD"/>
       </View>
       <View>
@@ -16,5 +16,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 10
+  },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%', 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 10
+  }
 });
