a9e8a39cec36e6c7856a537b88ee64b9f0ff30db
siddhant3030 <ssiddhant3030@gmail.com>
2019-12-26 14:58:16 +0530

    use of state

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
+
+
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  };
+
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input}/>
-        <Button title="ADD"/>
+        <TextInput
+          placeholder="Course Goal" 
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}/>
+        <Button title="ADD" onPress={addGoalHandler}/>
       </View>
       <View>
-
+        {courseGoals.map((goal) => <Text>{goal}</Text>)}
       </View>
     </View>
   );
