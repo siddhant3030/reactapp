4bc72140addc0f4e9d5bf452d54e109727ae1375
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-25 03:57:47 +0530

    database added

---
+++ meetup-backend/server/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+export default () => {
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/meetup');
+    mongoose.connection()
+            .once('open', () => console.log('mongodb running'))
+            .on('error', err => console.error(err));
+};
