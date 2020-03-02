33deff0986ecf724aa379fffac9fe07ccddb8a55
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-03 03:35:48 +0530

    reducer added

--- expense-tracker-react/src/context/GlobalState.js
+++ expense-tracker-react/src/context/GlobalState.js
@@ -1,4 +1,5 @@
 import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
 
 // Initial state
 
@@ -9,4 +10,22 @@ const initialState = {
     { id: 3, text: "Book", amount: -10 },
     { id: 4, text: "Camera", amount: 150 }
   ]
+};
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 };
