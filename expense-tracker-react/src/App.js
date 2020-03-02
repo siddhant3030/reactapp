7e13b643927fc41318a644414e2c568a6df630c3
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-02 13:07:00 +0530

    header component added

--- expense-tracker-react/src/App.js
+++ expense-tracker-react/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Header } from "./components/Header";
 import "./App.css";
 
 function App() {
-  return <div>HelloWorld</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
