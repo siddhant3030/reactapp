700908b23d0d3e0207f4caac07e260c5096ec17f
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-05 15:10:50 +0530

    provider added for binding react with redux

--- dailyplug/src/index.js
+++ dailyplug/src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { provider } from "react-redux";
 
-const store = createStore();
+const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
