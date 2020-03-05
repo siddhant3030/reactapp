b3b8f6f81a73316bfda54e05fe2913dc6992c63e
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-05 12:15:34 +0530

    project details added

---
+++ dailyplug/src/components/projects/ProjectDetails.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ProjectDetails = () => {
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project Title</span>
+          <p>asddsadsdsasdasds</p>
+        </div>
+      </div>
+    </div>
+  );
+};
