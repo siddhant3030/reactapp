f4227f7811a1b46cfd4e64dfe633491bace318cb
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-05 11:37:18 +0530

    project list

---
+++ dailyplug/src/components/projects/ProjectList.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <span className="card-title">Project Title</span>
+        <p>Posted by the net</p>
+        <span className="grey-text">30th Sep</span>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <span className="card-title">Project Title</span>
+        <p>Posted by the net</p>
+        <span className="grey-text">30th Sep</span>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <span className="card-title">Project Title</span>
+        <p>Posted by the net</p>
+        <span className="grey-text">30th Sep</span>
+      </div>
+    </div>
+  );
+};
