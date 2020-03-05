0d1d3e39c3997cabe3ec0b03c9a04f48c102a115
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-05 14:26:44 +0530

    create project component added

--- dailyplug/src/App.js
+++ dailyplug/src/App.js
@@ -4,7 +4,8 @@ import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/projects/ProjectDetails";
 import SignIn from "./components/auth/SignIn";
-import SignUp from "./components/auth/Sign/Up";
+import SignUp from "./components/auth/SignUp";
+import CreateProject from "./components/projects/CreateProject";
 
 class App extends Component {
   render() {
@@ -17,6 +18,7 @@ class App extends Component {
             <Route path="/project/:id" component={ProjectDetails} />
             <Route path="/signin" component={SignIn} />
             <Route path="/signup" component={SignUp} />
+            <Route path="/create" component={CreateProject} />
           </Switch>
         </div>
       </BrowserRouter>
