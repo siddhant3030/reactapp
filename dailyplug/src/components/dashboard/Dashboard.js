b0743a698113bb05800368ccdf1b3e70700ebb9f
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-05 20:05:28 +0530

    error fixed

--- dailyplug/src/components/dashboard/Dashboard.js
+++ dailyplug/src/components/dashboard/Dashboard.js
@@ -6,11 +6,12 @@ import { connect } from "react-redux";
 class Dashboard extends Component {
   render() {
     const { projects } = this.props;
+    console.log("projects", projects);
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList project={projects} />
+            <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
