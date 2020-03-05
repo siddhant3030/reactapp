f0a7126814b237f810dd2709c724c01ae3fd0730
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-05 14:08:09 +0530

    setting the state

--- dailyplug/src/components/auth/SignIn.js
+++ dailyplug/src/components/auth/SignIn.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 
 class SignIn extends Component {
-  state = {};
+  state = {
+    email: "",
+    password: ""
+  };
+
   handleChange = e => {
-    console.log(e);
+    this.setState({
+      [e.target.id]: e.target.value
+    });
   };
+
   handleSubmit = e => {
-    console.log(e);
+    e.preventDefault();
   };
+
   render() {
     return (
       <div className="container">
