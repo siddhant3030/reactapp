565ad68df39544a1667f309a590bfe77635407ab
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-05 12:03:30 +0530

    project summary created

--- dailyplug/src/components/projects/ProjectList.js
+++ dailyplug/src/components/projects/ProjectList.js
@@ -1,23 +1,13 @@
 import React from "react";
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <span className="card-title">Project Title</span>
-        <p>Posted by the net</p>
-        <span className="grey-text">30th Sep</span>
-      </div>
-      <div className="card z-depth-0 project-summary">
-        <span className="card-title">Project Title</span>
-        <p>Posted by the net</p>
-        <span className="grey-text">30th Sep</span>
-      </div>
-      <div className="card z-depth-0 project-summary">
-        <span className="card-title">Project Title</span>
-        <p>Posted by the net</p>
-        <span className="grey-text">30th Sep</span>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 };
---
+++ dailyplug/src/components/projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <span className="card-title">Project Title</span>
+      <p>Posted by the net</p>
+      <span className="grey-text">30th Sep</span>
+    </div>
+  );
+};
+
+export default ProjectSummary;
