7f7b0f10c938ca52db5efbb4e9f7ebd1ef39faaa
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-05 12:44:16 +0530

    props added in projectdetails

--- dailyplug/src/components/projects/ProjectDetails.js
+++ dailyplug/src/components/projects/ProjectDetails.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const ProjectDetails = () => {
+const ProjectDetails = props => {
+  const id = props.match.params.id;
   return (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project Title</span>
+          <span className="card-title">Project Title - {id}</span>
           <p>asddsadsdsasdasds</p>
         </div>
         <div className="card-action grey lighten-4 grey-text">
