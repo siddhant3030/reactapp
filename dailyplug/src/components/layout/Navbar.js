662bd06568d3dffad616312b5af5595c09238d66
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-05 11:10:10 +0530

    signed out links

--- dailyplug/src/components/layout/Navbar.js
+++ dailyplug/src/components/layout/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
+import SignedOutLinks from "./SignedOutLinks";
 
 const Navbar = () => {
   return (
@@ -10,6 +11,7 @@ const Navbar = () => {
           DailyPlug
         </Link>
         <SignedInLinks />
+        <SignedOutLink />
       </div>
     </nav>
   );
