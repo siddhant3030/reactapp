2344e26fe3c4f608678f3f248c86bde9e9a58ccc
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-15 00:11:54 +0530

    hooks and classcomponent both tried

--- hooksreact/src/App.js
+++ hooksreact/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <ClassCounter />
+    </div>
+  );
 }
 
 export default App;
