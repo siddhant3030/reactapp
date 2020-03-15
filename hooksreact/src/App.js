ebe4653ce1903cbb03f7420962fe5c1bd8aa2baa
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-15 16:39:53 +0530

    ant design added

--- hooksreact/src/App.js
+++ hooksreact/src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ClassCounter from "./components/ClassCounter";
+import HookCounter from "./components/HookCounter";
 
 function App() {
-  return (
-    <div className="App">
-      <ClassCounter />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
