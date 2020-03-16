f1288fef05998e5cdc579d3deb1588308e993038
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-16 19:45:03 +0530

    two container done

---
+++ codinova/src/components/Pricing/PriceMain.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export default class PriceMain extends Component {
+  render() {
+    return (
+      <div class="split left">
+        <ul>
+          <li>
+            <a href="default.asp">Product</a>
+          </li>
+          <li>
+            <a href="news.asp">Price</a>
+          </li>
+          <li>
+            <a href="contact.asp">Quantity</a>
+          </li>
+          <li>
+            <a href="about.asp">Title</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
---
+++ codinova/src/components/ProductList/ProductListMain.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ProductListMain extends Component {
+  render() {
+    return (
+      <div class="split right">
+        <h2>Siddhant</h2>
+      </div>
+    );
+  }
+}
